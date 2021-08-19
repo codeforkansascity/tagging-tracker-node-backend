6eaefb1c1470560bdcd94f45b7fe0dba7ba85d46
Jacob David C. Cunningham <jcun4128@gmail.com>
2021-08-19 08:51:20 -0500

    undo the aws cli changes

--- utils/sync/sync-up.js
+++ utils/sync/sync-up.js
@@ -9,7 +9,6 @@ const { makeRandomStr } = require('./../misc/stringGenerator');
 // import s3 stuff from module later
 const AWS = require('aws-sdk');
 const bucketName = process.env.AWS_S3_NAME;
-AWS.config.loadFromPath(__dirname + '/awsConfig.json');
 AWS.config.update({region: process.env.AWS_S3_REGION});
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
