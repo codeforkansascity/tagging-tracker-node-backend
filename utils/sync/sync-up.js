d0f5b0ba58eb6f9fe77282514e5086a1295296f5
Jacob David C. Cunningham <jcun4128@gmail.com>
2021-08-19 09:52:12 -0500

    tsk tsk

--- utils/sync/sync-up.js
+++ utils/sync/sync-up.js
@@ -11,8 +11,8 @@ const AWS = require('aws-sdk');
 const bucketName = process.env.AWS_S3_NAME;
 AWS.config.update({
     region: process.env.AWS_S3_REGION,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
 });
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
